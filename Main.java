6d60d5c19b98e486b8c891d0f441a5c25aa3b5cf
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-07-03 12:05:40 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,14 @@
-class Zip{
-  public static void main(String[] args){
-  }
+import java.io.*;
+public class Main {
+    public static void main(String[] args){
+        String text="Labdien!Ka tevi sauc?";
+        try(FileOutputStream fos=new FileOutputStream("C:/Users/Ilja/IdeaProjects/Zip/Java.txt")){
+            byte[] buffer = text.getBytes();
+            fos.write(buffer, 0, buffer.length);
+            System.out.println("The file has been written");
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 }
