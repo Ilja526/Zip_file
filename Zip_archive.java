ffb32d0ecea9731bded9e90947727fd24d3588b1
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-07-03 12:10:06 +0300

    Update Zip_archive.java

--- Zip_archive.java
+++ Zip_archive.java
@@ -7,12 +7,12 @@ public class Zip_archive{
             String name;
             long size;
             while((entry=zin.getNextEntry())!=null){
-                name = entry.getName();
+                name=entry.getName();
                 size=entry.getSize();
                 System.out.printf("File name: %s \t File size: %d \n", name, size);
 
-                FileOutputStream fout = new FileOutputStream("C:/Users/Ilja/IdeaProjects/Zip/Java4/new"+name);
-                for (int c = zin.read(); c != -1; c = zin.read()){
+                FileOutputStream fout=new FileOutputStream("C:/Users/Ilja/IdeaProjects/Zip/Java4/new"+name);
+                for (int c=zin.read(); c!=-1; c=zin.read()){
                     fout.write(c);
                 }
                 fout.flush();
